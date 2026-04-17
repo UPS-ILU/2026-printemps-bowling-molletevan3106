89ca42378dc90e030643a14c39fdc4e21db3eb5c
molletevan3106 <mollet.evan3106@gmail.com>
2026-04-17 12:40:23 +0200

    Iterération 2

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,12 +1,42 @@
 package tdd;
 
 public class Game {
-	private int somme=0;
-	void roll(int nb_quille) {
-		somme+=nb_quille;
-	}
-	int score() {
-		
-		return somme;
-	}
+    private int somme = 0;
+    private boolean bonus = false;
+
+    void roll(int premierEssai, int deuxiemeEssai) {
+        if (estSpare(premierEssai, deuxiemeEssai)) {
+            gererSpare(premierEssai, deuxiemeEssai);
+        } else if (bonus) {
+            gererBonus(premierEssai, deuxiemeEssai);
+        } else {
+            ajouterScoreNormal(premierEssai, deuxiemeEssai);
+        }
+    }
+
+    int score() {
+        return somme;
+    }
+    private boolean estSpare(int premierEssai, int deuxiemeEssai) {
+        return premierEssai != 10 && (premierEssai + deuxiemeEssai) == 10;
+    }
+
+    private void gererSpare(int premierEssai, int deuxiemeEssai) {
+        if (bonus) {
+            somme += premierEssai;
+        }
+        bonus = true;
+        somme += premierEssai + deuxiemeEssai;
+    }
+
+    private void gererBonus(int premierEssai, int deuxiemeEssai) {
+        somme += premierEssai;
+        somme += premierEssai + deuxiemeEssai;
+        bonus = false;
+    }
+
+    private void ajouterScoreNormal(int premierEssai, int deuxiemeEssai) {
+        somme += premierEssai + deuxiemeEssai;
+        bonus = false;
+    }
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -22,24 +22,33 @@ class GameTest {
 	@Test
 	void test_aucune_quille() {
 		for (int i = 0; i < 20; i++) {
-			game.roll(0);
+			game.roll(0,0);
 		}
 		assertEquals(0,game.score());
 	}
 	@Test
 	void test_une_quille() {
-		for (int i = 0; i < 20; i++) {
-			game.roll(1);
+		for (int i = 0; i < 10; i++) {
+			game.roll(1,1);
 			
 		}
 		assertEquals(20,game.score());
 	}
+	@Test
 	void test_uneEtdeux_quille() {
 		for (int i = 0; i < 10; i++) {
-			game.roll(1);
-			game.roll(2);
+			game.roll(1,2);
 			
 		}
 		assertEquals(30,game.score());
+	}
+	@Test
+	void test_spare() {
+		game.roll(7, 3);
+		game.roll(4, 0);
+		for (int i = 0; i < 8; i++) {
+			game.roll(0, 0);
+		}
+		assertEquals(18,game.score());
 	}
 }
